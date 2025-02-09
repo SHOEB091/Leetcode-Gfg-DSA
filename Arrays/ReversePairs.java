99d382be1199a68fe94b5cff0eb17d1aec04963e
shoeb <wiqbal091@gmail.com>
2025-02-09 23:03:53 +0530

    ReversePairs

--- Arrays/ReversePairs.java
+++ Arrays/ReversePairs.java
@@ -23,10 +23,63 @@ public class ReversePairs{
         }
         
         return count;
+    }    
+}
+    
+    //OPTIMAL APPROACH 
+    //merge sort approach 
+
+class Solution {
+    public int reversePairs(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        return mergeSort(nums, 0, nums.length - 1);
     }
 
-    
+    private int mergeSort(int[] nums, int left, int right) {
+        if (left >= right) return 0;
 
+        int mid = left + (right - left) / 2;
+        int count = mergeSort(nums, left, mid) + mergeSort(nums, mid + 1, right);
+        
+        // Count reverse pairs before merging
+        count += countPairs(nums, left, mid, right);
+        
+        // Merge the two sorted halves
+        merge(nums, left, mid, right);
+        
+        return count;
+    }
 
+    private int countPairs(int[] nums, int left, int mid, int right) {
+        int count = 0, j = mid + 1;
+        
+        // Count valid pairs (nums[i] > 2 * nums[j])
+        for (int i = left; i <= mid; i++) {
+            while (j <= right && (long) nums[i] > 2L * nums[j]) {
+                j++;
+            }
+            count += (j - (mid + 1));
+        }
+        
+        return count;
+    }
 
+    private void merge(int[] nums, int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left, j = mid + 1, k = 0;
+
+        while (i <= mid && j <= right) {
+            if (nums[i] <= nums[j]) {
+                temp[k++] = nums[i++];
+            } else {
+                temp[k++] = nums[j++];
+            }
+        }
+
+        while (i <= mid) temp[k++] = nums[i++];
+        while (j <= right) temp[k++] = nums[j++];
+        
+        // Copy back to original array
+        System.arraycopy(temp, 0, nums, left, temp.length);
+    }
 }
