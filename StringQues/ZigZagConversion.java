51ab328070b9d6fb83aa8aa6ff0827a0fb279e5e
shoeb <wiqbal091@gmail.com>
2025-05-30 00:40:48 +0530

    Zig Zag Conversion

---
+++ StringQues/ZigZagConversion.java
@@ -0,0 +1,23 @@
+package StringQues;
+
+class ZigZagConversion{
+
+    public String convert(String s, int n) {
+        if(s.length()==0||n==0) return null;
+        if(n==1) return s;
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<n;i++){
+            for(int j=i;j<s.length();j=j+2*(n-1)){
+                sb.append(s.charAt(j));
+                if(i>0 && i<n-1 && j+2*(n-1)-2*i<s.length()){
+                    sb.append(s.charAt(j+2*(n-1)-2*i));
+                }
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args){
+
+    }
+}
+
