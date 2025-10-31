03ba44e2f5d097091fb9e79fdc5cbc4f10d5f4b3
shoeb <wiqbal091@gmail.com>
2025-10-31 23:11:22 +0530

    logic building paterns

---
+++ Pattern/numberConsecutiveRightTriangle.java
@@ -0,0 +1,14 @@
+package Pattern;
+
+public class numberConsecutiveRightTriangle {
+    
+    public static void main(String[] args) {
+        for(int i=0;i<=5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(i+1);
+            }
+            System.out.println(" ");
+        }
+    }
+
+}
