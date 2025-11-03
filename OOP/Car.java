1cba5c6a52572b4d763d932d103d084ffe6d344e
shoeb <wiqbal091@gmail.com>
2025-11-03 18:04:05 +0530

    classes object added

--- OOP/Car.java
+++ OOP/Car.java
@@ -1,5 +1,55 @@
 package OOP;
 
 public class Car {
-    
+    // String color;
+    // String brand;
+    // String model;
+    // int manufYear;
+    // int speed;
+    private String color;
+    private String brand;
+    private String model;
+    private int manufYear;
+    private int speed;
+
+    public String getColor() {
+        return color;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public int getManufYear() {
+        return manufYear;
+    }
+    public void setManufYear(int manufYear) {
+        this.manufYear = manufYear;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+    public void accelerate(int increment){
+        speed+= increment;
+    }
+    public void brake(int decrement){
+        speed -= decrement;
+    }
+
+    //this is encapsulation as well binding data and methods in same class
+
+    //If all feilds are private then how can we access it we can access it by using getters and setters.
 }
---
+++ OOP/Test.java
@@ -0,0 +1,18 @@
+package OOP;
+
+public class Test {
+    public static void main(String[] args) {
+        Car car = new Car();
+        //car.setBrand("Mahindra");
+        // car.setColor("Black");
+        // car.setModel("Scorpio N");
+
+        // car.color="blue";
+        // car.manufYear= 2024;
+        // car.speed=120;
+        // car.model="safari";
+
+        
+        //System.out.println(car.getBrand());
+    }
+}
