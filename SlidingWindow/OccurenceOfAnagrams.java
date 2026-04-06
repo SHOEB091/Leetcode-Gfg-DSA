767269b7a81266f9c5798c0d6a0db43b32398a73
shoeb <wiqbal091@gmail.com>
2026-04-06 20:16:23 +0530

    OccurenceOfAnagrams

---
+++ SlidingWindow/OccurenceOfAnagrams.java
@@ -0,0 +1,19 @@
+package SlidingWindow;
+
+//Anagarm means matching leter ( reshuffle of for present in the given text like)
+/*
+for
+fro
+ofr
+orf
+rof
+rfo
+*/
+//Input: txt = forxxorfxdofr
+       //pat = for
+//Output = 3
+//Explanation in the given text for is present , orf is present ofr is also present
+
+public class OccurenceOfAnagrams {
+    
+}
