bd76c191931e2768b6b67aaafd1533e9246fe821
SHOEB IQBAL <98752195+SHOEB091@users.noreply.github.com>
2026-09-07 10:53:18 +0530

    Add AllPath class with Edge inner class

--- Graphs/AllPathAlgo.java
+++ Graphs/AllPathAlgo.java
@@ -1,1 +1,14 @@
+package Graphs;
 
+public class AllPath{
+  public static class Edge{
+    int source;
+    int destination;
+      public Edge(int source, int destination){
+        this.source = source;
+        this.destination = destination;
+      }
+  }
+  
+  
+}  
