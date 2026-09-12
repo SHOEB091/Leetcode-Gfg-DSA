8f43112cc617465104835111f654f059001380fe
shoeb <wiqbal091@gmail.com>
2026-09-12 20:41:00 +0530

    SortKSortedArray added

--- Heaps/SortKSortedArray.java
+++ Heaps/SortKSortedArray.java
@@ -4,27 +4,33 @@ import java.util.ArrayList;
 import java.util.PriorityQueue;
 
 public class SortKSortedArray {
-    public static ArrayList sortedArray(int[]arr , int k){
+    public static ArrayList<Integer> sortedArray(int[] arr, int k) {
         ArrayList<Integer> list = new ArrayList<>();
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
 
-        for(int i=0;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
+            // Push current element into min-heap
             minHeap.add(arr[i]);
-            if(minHeap.size()>k){
-                int ele = minHeap.poll();
-                list.add(ele);
+
+            // Keep heap size at k; smallest element moves to result
+            if (minHeap.size() > k) {
+                list.add(minHeap.poll());
             }
         }
-        while(!minHeap.isEmpty()){
-            int ele= minHeap.poll();
-            list.add(ele);
-        }
-        return list;
 
+        // Add remaining elements in sorted order
+        while (!minHeap.isEmpty()) {
+            list.add(minHeap.poll());
+        }
+
+        return list;
     }
+
     public static void main(String[] args) {
-        int arr[] = {6,5,3,2,8,10,9};
+        int arr[] = {6, 5, 3, 2, 8, 10, 9};
         int k = 3;
+
+        // Prints: [2, 3, 5, 6, 8, 9, 10]
         System.out.println(sortedArray(arr, k));
     }
 }
