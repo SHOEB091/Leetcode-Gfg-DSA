97b2e0ec3a7e0cf9ce9d524e89bc4df78b52782f
SHOEB IQBAL <98752195+SHOEB091@users.noreply.github.com>
2026-09-14 12:20:39 +0530

    Create kClosestNumber class with initial comments
    Added initial structure and comments for kClosestNumber class.

--- Heaps/kClosestNumber.java
+++ Heaps/kClosestNumber.java
@@ -1,1 +1,11 @@
-
+package Heaps;
+import java.util.PriorityQueue;
+// arr[] = { 5,6,7,8,9}
+// k = 3
+// x= 7
+// Approach 1 what we can do is subtract 7 from all the array elements less three numbers are the our mumbers
+// Then sort the number we get and add the corresponding element of that number in pair in heap
+// key -> absulote of (arr[i] - x)
+public class kClosestNumber{
+  
+}
