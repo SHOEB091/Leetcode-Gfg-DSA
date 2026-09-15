8c57593a5a5a85f9a7601b5b82a979b55c1e0254
SHOEB IQBAL <98752195+SHOEB091@users.noreply.github.com>
2026-09-15 13:00:10 +0530

    Add ConnectedRopesMinimiseCost class with cost calculation
    Implement a method to calculate the minimum cost of connecting ropes using a priority queue.

--- Heaps/ConnectedRopesMinimiseCost.java
+++ Heaps/ConnectedRopesMinimiseCost.java
@@ -1,1 +1,23 @@
+package Heaps;
 
+import java.util.PriorityQueue;
+
+public class ConnectedRopesMinimiseCost {
+    public int calculateMinimumCost(int arr[]) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        int cost = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            minHeap.add(arr[i]);
+        }
+
+        while (minHeap.size() >= 2) {
+            int first = minHeap.poll();
+            int second = minHeap.poll();
+            cost += first + second;
+            minHeap.add(first + second);
+        }
+
+        return cost;
+    }
+}
