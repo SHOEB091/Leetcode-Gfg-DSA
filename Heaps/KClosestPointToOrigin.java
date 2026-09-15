8ef220d642bfd2108a912fbaf2557422e450c439
SHOEB IQBAL <98752195+SHOEB091@users.noreply.github.com>
2026-09-15 12:16:10 +0530

    Fix Pair comparison and constructor in KClosestPointToOrigin
    Fix comparison logic in Pair class and correct Pair constructor parameters.

--- Heaps/KClosestPointToOrigin.java
+++ Heaps/KClosestPointToOrigin.java
@@ -1,1 +1,36 @@
+package Heaps;
 
+public class KClosestPointToOrigin{
+  public static class Pair implements Comparable<Pair>{
+    int key;
+    int value;
+    public Pair(int key , int value){
+      this.key = key;
+      this.value = value;
+    }
+    @Override
+    public int compareTo(int order){
+      return this.order - this.key;
+    }
+    
+  }
+  public int[][] kClosest(int[][] arr, int k) {
+    PriorityQueue<Pair> maxHeap = new PriorityQueue<>();
+
+    for (int i = 0; i < arr.length; i++) {
+        int d = arr[i][0] * arr[i][0] + arr[i][1] * arr[i][1];
+        maxHeap.add(new Pair(d, arr[i][0], arr[i][1]));
+        if (maxHeap.size() > k) maxHeap.poll();   // drop the farthest
+    }
+
+    int[][] res = new int[maxHeap.size()][2];
+    int idx = 0;
+    while (!maxHeap.isEmpty()) {
+        Pair p = maxHeap.poll();
+        res[idx][0] = p.x;
+        res[idx][1] = p.y;
+        idx++;
+    }
+    return res;
+}
+}
